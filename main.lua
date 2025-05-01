af4bf089330567f958161ce1bebb480c751394e1
gomlet674 <faiqabqoriahmad1@gmail.com>
2025-05-01 15:37:03 +0800

    main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,12 @@
--- GMON Main Loader Script -- Eksekusi awal, akan memuat GUI utama dari file gui.lua -- Pastikan key sudah diisi sebelum eksekusi
+-- GMON Main Loader Script 
+-- Eksekusi awal, akan memuat GUI utama dari file gui.lua 
+-- Pastikan key sudah diisi sebelum eksekusi
 
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
--- Masukkan key manual sebelum eksekusi (atau gunakan GUI get key terpisah jika diinginkan) getgenv().Key = "MASUKKAN_KEY_DISINI"
+-- Masukkan key manual sebelum eksekusi (atau gunakan GUI get key terpisah jika diinginkan) 
+getgenv().Key = "Bcd127aLt94dcp"
 
--- Load GUI loadstring(game:HttpGet("https://raw.githubusercontent.com/Gomlet674/gmonhub-script/main/gui.lua"))()
+-- Load GUI 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Gomlet674/gmonhub-script/main/gui.lua"))()
 
