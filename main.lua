62da6e0d90ec85a04b5f31a452b149dddf34684b
gomlet674 <faiqabqoriahmad1@gmail.com>
2025-05-01 20:28:08 +0800

    main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,47 @@
--- main.lua -- GMON Main Script Loader
+-- main.lua 
+-- GMON Main Scrip Loader 
 
--- Cek jika GUI sudah terbuka if game.CoreGui:FindFirstChild("GMON_GUI") then game.CoreGui:FindFirstChild("GMON_GUI"):Destroy() end
+-- Cek jika GUI sudah terbuka 
+if game.CoreGui:FindFirstChild("GMON_GUI") then game.CoreGui:FindFirstChild("GMON_GUI"):Destroy() end
 
--- Inisialisasi library dan GUI utama loadstring(game:HttpGet("https://raw.githubusercontent.com/gomlet674/gmonhub-script/main/gui.lua"))()
+-- Inisialisasi library dan GUI utama 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gomlet674/gmonhub-script/main/gui.lua"))()
 
--- Auto detection Sea/Level untuk Auto Farm local SeaLevel = game.Players.LocalPlayer.Data.Level.Value local CurrentSea = "First Sea" if SeaLevel >= 700 and SeaLevel < 1500 then CurrentSea = "Second Sea" elseif SeaLevel >= 1500 then CurrentSea = "Third Sea" end
+-- Auto detection Sea/Level untuk Auto Farm 
+local SeaLevel = game.Players.LocalPlayer.Data.Level.Value 
+local CurrentSea = "First Sea" 
+if SeaLevel >= 700 
+  and SeaLevel < 1500 then 
+  CurrentSea = "Second Sea" 
+elseif SeaLevel >= 1500 then 
+  CurrentSea = "Third Sea"
+end
 
 print("[GMON] Detected:", CurrentSea)
 
--- Memulai AutoFarm berdasarkan Sea spawn(function() while wait(5) do if getgenv().AutoFarm then pcall(function() if CurrentSea == "First Sea" then -- AutoFarm First Sea elseif CurrentSea == "Second Sea" then -- AutoFarm Second Sea elseif CurrentSea == "Third Sea" then -- AutoFarm Third Sea end end) end end end)
+-- Memulai AutoFarm berdasarkan Sea 
+spawn(function()
+    while wait(5) do 
+      if getgenv().AutoFarm then 
+        pcall(function() 
+            if CurrentSea == "First Sea" then
+              -- AutoFarm First Sea 
+            elseif CurrentSea == "Second Sea" then 
+              -- AutoFarm Second Sea 
+            elseif CurrentSea == "Third Sea" then 
+              -- AutoFarm Third Sea 
+            end 
+          end) 
+      end 
+    end 
+  end)
 
--- Inisialisasi notifikasi GMON Style function Notify(msg) game.StarterGui:SetCore("SendNotification", { Title = "GMON", Text = msg, Duration = 4 }) end
+-- Inisialisasi notifikasi GMON Style 
+function Notify(msg) game.StarterGui:SetCore("SendNotification", { 
+      Title = "GMON", 
+      Text = msg, 
+      Duration = 4 }
+  ) end
 
 Notify("GMON GUI Loaded - Welcome!")
 
